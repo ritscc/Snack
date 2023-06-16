6b1051d6b38331cb4fe5269c4c78111b8f83a41a
クマさん <68993998+CityBear3@users.noreply.github.com>
2023-06-16 18:15:10 +0900

    環境変数取得関数を実装 (#74)

---
+++ backend/driver/env/env_loader.go
@@ -0,0 +1,39 @@
+package env
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// ValueNotFoundError error message for value not found
+const ValueNotFoundError = "%s is not found. this environment value must be set."
+
+// GetStrEnv get value
+func GetStrEnv(key string, required bool) (string, error) {
+	v, exist := os.LookupEnv(key)
+	if required && !exist {
+		return "", errors.New(fmt.Sprintf(ValueNotFoundError, key))
+	}
+
+	return v, nil
+}
+
+// GetIntEnv get int value
+func GetIntEnv(key string, required bool) (int, error) {
+	v, exists := os.LookupEnv(key)
+	if !exists {
+		if required {
+			return 0, errors.New(fmt.Sprintf(ValueNotFoundError, key))
+		}
+		return 0, nil
+	}
+
+	n, err := strconv.Atoi(v)
+	if err != nil {
+		return 0, err
+	}
+
+	return n, nil
+}
--- backend/go.mod
+++ backend/go.mod
@@ -3,14 +3,18 @@ module github.com/ritscc/Snack
 go 1.20
 
 require (
+	github.com/stretchr/testify v1.8.4
 	google.golang.org/grpc v1.55.0
 	google.golang.org/protobuf v1.30.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
