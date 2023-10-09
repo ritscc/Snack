aa7b9512b2d70e0fcf94483ad9da82683f389b72
rintaro1029 <tomitaka1029@gmail.com>
2023-10-09 19:26:40 +0900

    ssrの無効化

--- desktop/src/pages/_app.tsx
+++ desktop/src/pages/_app.tsx
@@ -1,28 +1,31 @@
+import CSR from "@/components/CSR";
 import Header from "@/components/Header";
 import Online from "@/components/Online";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const isAuthPage = router.pathname.includes("/auth");
 
   return (
-    <div className="w-full">
-      {!isAuthPage && (
-        <div className="sticky top-0 ">
-          <Header />
-        </div>
-      )}
+    <CSR>
+      <div className="w-full">
+        {!isAuthPage && (
+          <div className="sticky top-0 ">
+            <Header />
+          </div>
+        )}
 
-      <div className="flex h-screen ">
-        {!isAuthPage && <Sidebar />}
-        <Component {...pageProps} />
-        {!isAuthPage && <Online />}
+        <div className="flex h-screen ">
+          {!isAuthPage && <Sidebar />}
+          <Component {...pageProps} />
+          {!isAuthPage && <Online />}
+        </div>
       </div>
-    </div>
+    </CSR>
   );
 }
