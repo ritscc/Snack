6f02d61572b7b6b0e0f9db5232f539e5d218b8f7
rintaro1029 <tomitaka1029@gmail.com>
2023-11-23 00:43:13 +0900

    create input message function

--- desktop/src/components/chat/Chat.tsx
+++ desktop/src/components/chat/Chat.tsx
@@ -3,40 +3,47 @@ import Loading from "../Loading";
 import { Skeleton } from "../ui/skeleton";
 import Image from "next/image";
 import { ThumbsUp } from "lucide-react";
+import { Message } from "@/api/message";
 
-const Chat = () => {
+interface ChatProps {
+  messages: Message[];
+}
+
+const Chat = ({ messages }: ChatProps) => {
   return (
     <div className="hide-scrollbar h-screen w-full overflow-y-auto   bg-chat">
       {/* <Loading /> */}
       <div className="ml-3 space-y-4">
-        <div className="flex items-start space-x-4">
-          <Image
-            src="/images/rcc-favicon.png"
-            alt="RCCのfaviconです"
-            width={40}
-            height={40}
-            className="h-12 w-12 rounded-full "
-          />
+        {messages.map((message) => (
+          <div key={message.message_id} className="flex items-start space-x-4">
+            <Image
+              src="/images/rcc-favicon.png"
+              alt="RCCのfaviconです"
+              width={40}
+              height={40}
+              className="h-12 w-12 rounded-full "
+            />
 
-          <div className="space-y-2">
-            {/* ここにユーザー名を表示 */}
-            <div className="flex">
-              <p className="h-4 w-[100px]">RCC太郎</p>
-              <p className="text-xs">2023/10/29</p>
-            </div>
+            <div className="space-y-2">
+              {/* ここにユーザー名を表示 */}
+              <div className="flex">
+                <p className="h-4 w-[100px]">RCC太郎</p>
+                <p className="text-xs">{new Date(message.created_time).toLocaleDateString()}</p>
+              </div>
 
-            <div className="flex space-x-1">
-              {/* ここにメッセージを表示 */}
-              本日の例会は18:00からです.場所はF106です
-            </div>
-            <div className="flex space-x-1">
-              {/* ここにえもじを表表示 */}
-              <ThumbsUp className="text-yellow-500" />
-              {/* 言い値数を表示する */}
-              <span>4</span>
+              <div className="flex space-x-1">
+                {/* ここにメッセージを表示 */}
+                {message.content}
+              </div>
+              <div className="flex space-x-1">
+                {/* ここにえもじを表表示 */}
+                <ThumbsUp className="text-yellow-500" />
+                {/* 言い値数を表示する */}
+                <span>4</span>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
