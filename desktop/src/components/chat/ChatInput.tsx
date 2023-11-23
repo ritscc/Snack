cf932a9aa76a95dedc67356601e4ec061ad19a92
RINTARO TOMITAKA <111993102+rintaro29@users.noreply.github.com>
2023-11-23 18:09:32 +0900

    Create Chat UI (#106)

--- desktop/src/components/chat/ChatInput.tsx
+++ desktop/src/components/chat/ChatInput.tsx
@@ -1,12 +1,31 @@
 import { Laugh, PlusCircle } from "lucide-react";
 import { Input } from "../ui/input";
+import { useState } from "react";
 
-const ChatInput = () => {
+interface ChatInputProps {
+  sendNewMessage: (message: string) => void;
+}
+
+const ChatInput = ({ sendNewMessage }: ChatInputProps) => {
+  const [inputText, setInputText] = useState("");
+  const handleSendMessage = () => {
+    if (inputText.trim()) {
+      sendNewMessage(inputText);
+      setInputText("");
+    }
+  };
+
   return (
     <div className="flex justify-between sticky bottom-1">
       <div className="relative flex items-center m-2  w-full ">
         <PlusCircle className="text-slate-400 absolute ml-2" />
-        <Input className="m-1 pl-8 bg-slate-100 " placeholder="" />
+        <Input
+          className="m-1 pl-8 bg-slate-100 "
+          placeholder=""
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleSendMessage()}
+        />
         <Laugh className="right-2 absolute text-slate-400 " />
       </div>
     </div>
