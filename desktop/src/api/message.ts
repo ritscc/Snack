a5d3c5b13bdb9944f3b8c097b631c14a4f375a4f
rintaro1029 <tomitaka1029@gmail.com>
2023-11-26 14:28:45 +0900

    Revert "SWRの導入"
    This reverts commit b6970f9d18d352718df9fb5a7d395682aa608205.

--- desktop/src/api/message.ts
+++ desktop/src/api/message.ts
@@ -1,5 +1,4 @@
 import { invoke } from "@tauri-apps/api/tauri";
-import useSWR from "swr";
 
 export interface Mention {
   everyone: boolean;
@@ -20,27 +19,14 @@ export interface Message {
   stamp_id: number; // Vec<i64> は TypeScript で number[]
 }
 
-// メッセージを取得する関数
-const fetchMessages = (): Promise<Message[]> => invoke("get_test_messages");
-
-// メッセージを取得するカスタムフック(SWR
-export function useMessages() {
-  const { data, error } = useSWR<Message[]>("get_test_messages", fetchMessages);
-
-  return {
-    messages: data,
-    isLoading: !error && !data,
-    isError: !!error,
-  };
+async function get_test_messages() {
+  try {
+    const messages = await invoke<Message[]>("get_test_messages");
+    return messages;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 }
 
-//useEffect用
-// async function get_test_messages() {
-//   try {
-//     const messages = await invoke<Message[]>("get_test_messages");
-//     return messages;
-//   } catch (err) {
-//     console.error(err);
-//     return null;
-//   }
-// }
+export default get_test_messages;
