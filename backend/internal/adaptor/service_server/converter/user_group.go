1d9049cf562ef09afe69f8cea9fd1834771fd7d5
hane <winwinmaomao@gmail.com>
2023-11-29 18:15:12 +0900

    converterを作成

--- backend/internal/adaptor/service_server/converter/user_group.go
+++ backend/internal/adaptor/service_server/converter/user_group.go
@@ -6,15 +6,41 @@ import (
 )
 
 func UserGroupTopbUserGroup(userGroup *model.UserGroup) *pbUserGroup.UserGroup {
+	var members []*pbUserGroup.Member
+	for _, member := range userGroup.Member {
+		members = append(members, &pbUserGroup.Member{
+			UserId: member.UserId,
+			Grant:  member.Grant,
+		})
+	}
+
 	return &pbUserGroup.UserGroup{
-		GroupId:    userGroup.GroupID,
+		GroupId:     userGroup.GroupID,
 		Name:        userGroup.Name,
 		Description: userGroup.Description,
 		Type:        userGroup.Type,
 		Icon:        userGroup.Icon,
 		Role:        userGroup.Role,
-		Member:      converter.MemberTopbMember(userGroup.Member),
+		Member:      members,
 	}
 }
 
-func 
+func PbUserGroupToUserGroup(userGroup *pbUserGroup.UserGroup) *model.UserGroup {
+	var members []*model.Member
+	for _, member := range userGroup.Member {
+		members = append(members, &model.Member{
+			UserId: member.UserId,
+			Grant:  member.Grant,
+		})
+	}
+
+	return &model.UserGroup{
+		GroupID:     userGroup.GroupId,
+		Name:        userGroup.Name,
+		Description: userGroup.Description,
+		Type:        userGroup.Type,
+		Icon:        userGroup.Icon,
+		Role:        userGroup.Role,
+		Member:      members,
+	}
+}
