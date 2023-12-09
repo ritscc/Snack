afed9ce865309fe7bbc294230aa22c0b3e2dec46
hane <winwinmaomao@gmail.com>
2023-12-09 23:13:17 +0900

    repositoryの追加

--- backend/internal/adaptor/repository/user.go
+++ backend/internal/adaptor/repository/user.go
@@ -13,6 +13,24 @@ func NewUserRepository() *UserRepository {
 }
 
 func (u UserRepository) FindUserByID(ctx context.Context, id int64) (*model.User, error) {
-	//TODO implement me
-	panic("implement me")
+	//TODO: test
+	return &model.User{
+		UserID:    id,
+		UserName:  "Ritsumei Tarou",
+		Nick:      "USA",
+		RealName:  "Ritsumei Tarou",
+		Password:  [64]byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08},
+		Avatar:    "user.png",
+		Role:      "admin",
+		Locale:    "ja",
+		RitsEmail: "is0000aa@ed.ritsumei.ac.jp",
+		OwnEmail:  "ritsumei@gmail.com",
+		Comment:   "Hello, World!",
+		Status:    0,
+		Services: &model.Service{
+			Twitter: "https://twitter.com/ritscc",
+			Github:  "https://github/ritscc",
+			Discord: "https://discord.gg/ritscc",
+		},
+	}, nil
 }
